915c334b840ca68a8d370d357d7b5e2a1ac6ae94
uniksolana <unik.solana@gmail.com>
2026-06-03 15:35:03 +0200

    build: Configure Next.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unavatar.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
+  },
+  experimental: {
+    esmExternals: true,
+  },
+};
+
+module.exports = nextConfig;
